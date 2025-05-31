35616dbe4e46ba0cb01ba3506b1d7bfdc5a620a3
congmanh18 <nguyenmanh180102@gmail.com>
2025-05-31 21:31:37 +0700

    backend register login

--- backendpython/main.py
+++ backendpython/main.py
@@ -1,13 +1,9 @@
-# main.py
-
 from fastapi import FastAPI
+from routers import auth
+from database import engine, Base
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Chào mừng đến với backend Python!"}
-
-@app.get("/hello/{name}")
-def say_hello(name: str):
-    return {"message": f"Xin chào {name}!"}
+app.include_router(auth.router)
